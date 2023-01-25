31690fbe71aee7a3ed9b22b8ee35b7ca4eafd753
Craig <indigomx9@gmail.com>
2023-01-25 14:35:12 -0500

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# PERN App
+
+Initial setup of the project.  Created with Webpack, TypeScript, React, React Router, Supabase, and React Query.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Colton from "@public/Colton McKivitz1.webp";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <img 
+                src={Colton} alt="Colton McKivitz" 
+                height="600px" width="auto"
+            />
+        </React.Fragment>
+    );
+};
+
+
